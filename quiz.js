eda1f323d793cf8c1d6da65e4f04fa3f9199a78c
Sudip <sudipghevade15@gmail.com>
2023-07-07 13:05:43 +0530

    commit1

---
+++ quiz.js
@@ -0,0 +1,58 @@
+let questions = [
+    {
+    numb: 1,
+    question: ".MOV' extension refers usually to what kind of file?",
+    answer: "Animation/movie file",
+    options: [
+      "Image file",
+      "Animation/movie file",
+      "Audio file",
+      "MS office document"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which was the 1st non Test playing country to beat India in an international match?",
+    answer: "srilanka",
+    options: [
+      "Canada",
+      "srilanka",
+      "zimbanve",
+      "East africa"
+    ]
+  },
+    {
+    numb: 3,
+    question: "'Dandia' is a popular dance of?",
+    answer: "Gujrat",
+    options: [
+      "Punjab",
+      "Gujrat",
+      "Maharashtra",
+      "kerala"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+ 
+];
